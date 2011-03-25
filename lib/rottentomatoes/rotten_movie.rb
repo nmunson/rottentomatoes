8158f34d148e032709382a7194cb6293c92e991c
nmunson <nmunson@gmail.com>
2011-03-24 21:03:20 -0400

    Fix tabbing

--- lib/rottentomatoes/rotten_movie.rb
+++ lib/rottentomatoes/rotten_movie.rb
@@ -3,24 +3,24 @@ class RottenMovie
   def self.find(term, options = {})
     raise ArgumentError, "You must provide a search term" if (term.nil? || term.empty?)
 
-		results = []
-		results << RottenTomatoes.api_call("movies", term)
-		
-		results.flatten!
-		results.compact!
-	  
+    results = []
+    results << RottenTomatoes.api_call("movies", term)
+
+    results.flatten!
+    results.compact!
+
     unless (options[:limit].nil?)
       raise ArgumentError, "Limit must be an integer greater than 0" if (!options[:limit].is_a?(Fixnum) || !(options[:limit] > 0))
       results = results.slice(0, options[:limit])
     end
 
-		results.map!{|m| RottenMovie.new(m)}
+    results.map!{|m| RottenMovie.new(m)}
 
-		if (results.length == 1)
-			return results[0]
-		else
-			return results
-		end		
+    if (results.length == 1)
+      return results[0]
+    else
+      return results
+    end		
   end
 
   def self.new(raw_data)
